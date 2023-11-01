869edf753a44df481bc20a62abfb3f22362041a0
manjubhaskar02 <manju.bhaskar.jaya@gmail.com>
2023-11-01 13:37:06 +0530

    added favicon

---
+++ javascript.js
@@ -0,0 +1,6 @@
+// const urlParams = new URLSearchParams(window.location.search);
+// const myParam = urlParams.get('username');
+// if (myParam) {
+//     const ex = document.getElementById('aboutme')
+//     ex.innerText += myParam;
+// }
