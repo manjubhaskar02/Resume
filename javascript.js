3dcd73e8f5661a1a4677e435d42fbe52509ce8e9
manjubhaskar02 <manju.bhaskar.jaya@gmail.com>
2023-12-29 11:08:18 +0530

    removed the profile picture

--- javascript.js
+++
@@ -1,6 +0,0 @@
-// const urlParams = new URLSearchParams(window.location.search);
-// const myParam = urlParams.get('username');
-// if (myParam) {
-//     const ex = document.getElementById('aboutme')
-//     ex.innerText += myParam;
-// }
